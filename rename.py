83190e856c35523b6320fb968f5c3e6bfef5f5b3
SifonFelipe <51344022+SifonFelipe@users.noreply.github.com>
2024-07-30 16:28:31 -0300

    v1.1
    Added progress bar, absolute path, list of ignored files and total of files renamed and ignored

--- rename.py
+++ rename.py
@@ -1,6 +1,11 @@
 import os
+from tqdm import tqdm
 
 PYTHON_IGNORE = True #if you want to modify .py files, set this to False
+extensions_to_ignore = [ # add every file extension you want to ignore
+    #'.pdf',
+    #'.jpeg',
+]
 
 def is_file(directory, filename): #check if it is a directory or a .py file
     if os.path.isfile(os.path.join(directory, filename)) == False:
@@ -8,6 +13,9 @@ def is_file(directory, filename): #check if it is a directory or a .py file
 
     _, file_extension = os.path.splitext(filename)
     if file_extension.lower() == '.py' and PYTHON_IGNORE:
+        return False
+
+    if file_extension.lower() in extensions_to_ignore:
         return False
 
     return True
@@ -24,14 +32,15 @@ def rename_files(directory, prefix):
     The logic of this is to first split and rename the files that have a name like: prefix_[a number]
     from the others who cannot be overwritten.
     """
-    files = [filename for filename in os.listdir(directory) if is_file(directory, filename)] #split files from directories
+    files = sorted([filename for filename in os.listdir(directory) if is_file(directory, filename)]) #split files from directories
 
     file_index = 0 #prefix addon
 
     prefix += '_'
     num_sort = [] #list of the files with prefixs, only numbers to be able to sort
     secondary = [] #list of the files who cannot be overwritten
-    for filename in files:
+
+    for filename in files: #check for prefixed files
         if prefix in filename:
             splitted_filename = filename.split('_')
             if len(splitted_filename) == 2: #fake prefix (prefix_asd_31)
@@ -45,25 +54,29 @@ def rename_files(directory, prefix):
         else:
             secondary.append(filename)
 
+
     num_sort = sorted(num_sort)
     for number_of_file in num_sort:
         filename = prefix + str(number_of_file) #num_sort has the names of the files who have the prefix given, so i can get the name like this
         new_filename = prefix + str(file_index)
+        replace_file(directory, filename, new_filename)
         file_index += 1
 
-        replace_file(directory, filename, new_filename)
-        print(f'{file_index}/{len(files)}')
-
-    for filename in secondary:
+    for filename in tqdm(secondary, desc='Renaming remaining files'):
         new_filename = prefix + str(file_index)
         replace_file(directory, filename, new_filename)
         file_index += 1
-        print(f'{file_index}/{len(files)}')
 
-    print('done')
+    print('Done.')
+    print(f'Renamed Files: {len(files)}')
+    ignored_files = len(os.listdir(directory)) - len(files) if len(os.listdir(directory)) - len(files) > 0 else 0
+    print(f'Ignored files or directories: {ignored_files}')
 
 
-directory_path = str(input('Enter the path of your directory (you can use ./):\n'))
+directory_path = str(input('Enter the path of your directory (you can use ./ (relative path) or ~/ (absolute path):\n'))
+directory_path = os.path.expanduser(directory_path)
+
 prefix = str(input('Enter the prefix of the files (photo, backup, etc):\n'))
+print('\n')
 
 rename_files(directory_path, prefix)
