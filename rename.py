36a494cda55e81a59a189bcc467eb0a8b30cfcde
SifonFelipe <51344022+SifonFelipe@users.noreply.github.com>
2024-07-29 22:21:55 -0300

    v1.0

---
+++ rename.py
@@ -0,0 +1,69 @@
+import os
+
+PYTHON_IGNORE = True #if you want to modify .py files, set this to False
+
+def is_file(directory, filename): #check if it is a directory or a .py file
+    if os.path.isfile(os.path.join(directory, filename)) == False:
+        return False
+
+    _, file_extension = os.path.splitext(filename)
+    if file_extension.lower() == '.py' and PYTHON_IGNORE:
+        return False
+
+    return True
+
+def replace_file(directory, old_filename, new_filename):
+    old_path = os.path.join(directory, old_filename)
+    new_path = os.path.join(directory, new_filename)
+    os.rename(old_path, new_path)
+
+def rename_files(directory, prefix):
+    """
+    It's a bit long because there is a problem -> if you have photos or files with the prefix,
+    they will be overwritten when the files are renamed. sorted() does not solve the problem.
+    The logic of this is to first split and rename the files that have a name like: prefix_[a number]
+    from the others who cannot be overwritten.
+    """
+    files = [filename for filename in os.listdir(directory) if is_file(directory, filename)] #split files from directories
+
+    file_index = 0 #prefix addon
+
+    prefix += '_'
+    num_sort = [] #list of the files with prefixs, only numbers to be able to sort
+    secondary = [] #list of the files who cannot be overwritten
+    for filename in files:
+        if prefix in filename:
+            splitted_filename = filename.split('_')
+            if len(splitted_filename) == 2: #fake prefix (prefix_asd_31)
+                try:
+                    number_prefix = int(splitted_filename[-1]) #to int because it could be prefix_a
+                    num_sort.append(number_prefix)
+                except:
+                    secondary.append(filename)
+            else:
+                secondary.append(filename)
+        else:
+            secondary.append(filename)
+
+    num_sort = sorted(num_sort)
+    for number_of_file in num_sort:
+        filename = prefix + str(number_of_file) #num_sort has the names of the files who have the prefix given, so i can get the name like this
+        new_filename = prefix + str(file_index)
+        file_index += 1
+
+        replace_file(directory, filename, new_filename)
+        print(f'{file_index}/{len(files)}')
+
+    for filename in secondary:
+        new_filename = prefix + str(file_index)
+        replace_file(directory, filename, new_filename)
+        file_index += 1
+        print(f'{file_index}/{len(files)}')
+
+    print('done')
+
+
+directory_path = str(input('Enter the path of your directory (you can use ./):\n'))
+prefix = str(input('Enter the prefix of the files (photo, backup, etc):\n'))
+
+rename_files(directory_path, prefix)
