8e2111015c43c400b4ce7377e4450e5c61c99080
SifonFelipe <51344022+SifonFelipe@users.noreply.github.com>
2024-07-30 21:03:44 -0300

    v1.2
    Fixed an error where when changing the name of the file, it lost its extension. Also not allowing invalid paths. Moreover a feature that sets numerical names to the files if the prefix is null.

--- pyrenamer.py
+++ pyrenamer.py
@@ -7,7 +7,11 @@ extensions_to_ignore = [ # add every file extension you want to ignore
     #'.jpeg',
 ]
 
-def is_file(directory, filename): #check if it is a directory or a .py file
+def is_file(directory, filename):
+    """
+    Check if the path is a file. You can add your parameters of which type of file you don't want
+    to be modified in the variable extensions_to_ignore
+    """
     if os.path.isfile(os.path.join(directory, filename)) == False:
         return False
 
@@ -20,8 +24,10 @@ def is_file(directory, filename): #check if it is a directory or a .py file
 
     return True
 
-def replace_file(directory, old_filename, new_filename):
+def replace_file(directory, old_filename, new_filename): #replaces the files
     old_path = os.path.join(directory, old_filename)
+    _, file_extension = os.path.splitext(old_filename) # gets the extension of the file to add it to the name, because the file would lose the .
+    new_filename += file_extension # adding the extension to the name
     new_path = os.path.join(directory, new_filename)
     os.rename(old_path, new_path)
 
@@ -36,23 +42,36 @@ def rename_files(directory, prefix):
 
     file_index = 0 #prefix addon
 
-    prefix += '_'
+    if prefix != '':
+        prefix += '_'
+        type_pf = str()
+    else:
+        type_pf = int() # type int for prefixs nulls
+
     num_sort = [] #list of the files with prefixs, only numbers to be able to sort
     secondary = [] #list of the files who cannot be overwritten
 
     for filename in files: #check for prefixed files
-        if prefix in filename:
-            splitted_filename = filename.split('_')
-            if len(splitted_filename) == 2: #fake prefix (prefix_asd_31)
-                try:
-                    number_prefix = int(splitted_filename[-1]) #to int because it could be prefix_a
-                    num_sort.append(number_prefix)
-                except:
+
+        if type_pf != int(): #if the prefix is empty, the idea is to name the files with only a number
+            if prefix in filename:
+                splitted_filename = filename.split('_')
+                if len(splitted_filename) == 2: #fake prefix (prefix_asd_31)
+                    try:
+                        number_prefix = int(splitted_filename[-1]) #to int because it could be prefix_a
+                        num_sort.append(number_prefix)
+                    except:
+                        secondary.append(filename)
+                else:
                     secondary.append(filename)
             else:
                 secondary.append(filename)
         else:
-            secondary.append(filename)
+            try:
+                filename = int(filename) # same logic as before, if it can be overwritten, rename them first
+                num_sort.append(filename)
+            except:
+                secondary.append(filename)
 
 
     num_sort = sorted(num_sort)
@@ -72,9 +91,13 @@ def rename_files(directory, prefix):
     ignored_files = len(os.listdir(directory)) - len(files) if len(os.listdir(directory)) - len(files) > 0 else 0
     print(f'Ignored files or directories: {ignored_files}')
 
-
-directory_path = str(input('Enter the path of your directory (you can use ./ (relative path) or ~/ (absolute path):\n'))
-directory_path = os.path.expanduser(directory_path)
+while True:
+    directory_path = str(input('Enter the path of your directory (you can use ./ (relative path) or ~/ (absolute path):\n'))
+    directory_path = os.path.expanduser(directory_path)
+    if os.path.exists(directory_path): # checks if the path is valid
+        break
+    else:
+        print('The path assigned doesn`t exists')
 
 prefix = str(input('Enter the prefix of the files (photo, backup, etc):\n'))
 print('\n')
